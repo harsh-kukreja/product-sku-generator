1e4adc91ef63942cf17eb4330aa6a990226b9008
Harsh Kukreja <harshkukreja99@gmail.com>
2021-05-10 19:12:04 +0530

    refactor: added sku updated constant

--- app/Http/Controllers/Constants/ProductPermuteControllerConstants.php
+++ app/Http/Controllers/Constants/ProductPermuteControllerConstants.php
@@ -6,4 +6,5 @@ namespace App\Http\Controllers\Constants;
 
 interface ProductPermuteControllerConstants {
     public const SKU_DELETED_SUCCESSFULLY = 'SKU Deleted Successfully';
+    public const SKU_UPDATE_SUCCESSFULLY = 'SKU updated successfully';
 }
