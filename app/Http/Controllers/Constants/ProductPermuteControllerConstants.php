84426e636bbfc0583ac75a2992cfda061a483e49
Harsh Kukreja <harshkukreja99@gmail.com>
2021-05-10 19:27:25 +0530

    refactor: added constants to show the message if no description and image

--- app/Http/Controllers/Constants/ProductPermuteControllerConstants.php
+++ app/Http/Controllers/Constants/ProductPermuteControllerConstants.php
@@ -7,4 +7,8 @@ namespace App\Http\Controllers\Constants;
 interface ProductPermuteControllerConstants {
     public const SKU_DELETED_SUCCESSFULLY = 'SKU Deleted Successfully';
     public const SKU_UPDATE_SUCCESSFULLY = 'SKU updated successfully';
+
+    public const CLICK_ON_EDIT_FOR_DESCRIPTION = "Please Click on edit button to add description.";
+
+    public const CLICK_ON_EDIT_FOR_ADD_IMAGE = "Please Click on edit button to add image.";
 }
