1f8965a9299ca1405a3185ff299629012a37f1e7
Harsh Kukreja <harshkukreja99@gmail.com>
2021-05-10 06:19:47 +0530

    refactor: js file for create product page added

---
+++ public/js/product/create.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+    let counter = 0;
+    $('.option').hide()
+
+    $("#add-product").submit(function () {
+        $(".loader").addClass('d-block')
+        $(".loader").removeClass('d-none')
+        $(".loader-submit").addClass('d-none')
+        $(".loader-submit").removeClass('d-block')
+
+        $("<input type='hidden' />").attr("name", "counter").attr("value", counter).appendTo("#add-product");
+        return true
+    })
+
+    initializeSelect2()
+    const  addSelect2 = (counter)  =>  `<div class="row mt-3 remove-${counter}" >
+                                               <div class="col-4">
+                                                   <input type="text" class="form-control" name="option_name_${counter}"
+                                                       id="option_name"
+                                                       required
+                                                       placeholder="Enter Option Name">
+                                               </div>
+                                               <div class="col-6">
+                                                   <select class="option_value form-control" multiple
+                                                           id="option_value"
+                                                           required
+                                                           name="option_values_${counter}[]">
+                                                   </select>
+                                               </div>
+                                                <div class="col-2 ">
+                                                   <button class="btn btn-primary remove-option-${counter}" id="remove-option"
+                                                           type="button">X</button>
+                                               </div>
+                                           </div>`
+
+    $("#add-option").click(function () {
+        $('.option-content').append(addSelect2(counter++));
+        initializeSelect2()
+    })
+
+    $(`.option-content`).on("click", `#remove-option`, (function () {
+        counter--;
+        if (counter === 0) {
+            $('.option').hide()
+            $("input[name$='is_variant']").prop('checked', false); // Unchecks it
+        }
+        $(this).parent().parent().remove()
+    }))
+    $("input[name$='is_variant']").click(function () {
+        if ($("input:radio[name='is_variant']:checked").val() === "1") {
+            $('.option').show()
+            $('.option-content').append(addSelect2(counter++));
+            initializeSelect2()
+        } else  {
+            $('.option').hide()
+            $('.option-content').empty()
+        }
+    });
+})
+
+
+function initializeSelect2() {
+    $('.option_value').select2({
+        placeholder: 'Type option values',
+        width: "100%",
+        tags: true,
+        tokenSeparators: [' ']
+    })
+}
