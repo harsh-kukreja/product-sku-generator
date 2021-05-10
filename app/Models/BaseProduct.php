57a805a9d34b338a27c463008a1473468a860371
Harsh Kukreja <harshkukreja99@gmail.com>
2021-05-10 22:21:48 +0530

    refactor: adding relation

--- app/Models/BaseProduct.php
+++ app/Models/BaseProduct.php
@@ -2,7 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class BaseProduct extends BaseModel {
     public const VARIANT = 1;
     public const NO_VARIANT  = 0;
+
+    /**
+     * Adding Relation of product permute
+     * @return HasMany
+     */
+    public function productPermutes(): HasMany {
+        return $this->hasMany(ProductPermute::class);
+    }
 }
