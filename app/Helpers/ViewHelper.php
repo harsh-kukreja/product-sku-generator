015dbab4f2271db9c0cacbd803c8a2c803443987
Harsh Kukreja <harshkukreja99@gmail.com>
2021-05-10 06:18:59 +0530

    refactor: adding a helper class to return when using data tables

---
+++ app/Helpers/ViewHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Helpers;
+
+
+class ViewHelper {
+    public static function controlLinkButton($icon_code, $color, $id, $link, $class_name, $text="") {
+        return '<a href="'.$link.'" id="'.$id.'" class="'.$class_name.' '.$icon_code.' btn '.$color.'">'.$text.'</a>';
+    }
+
+    public static function controlModalButton($icon_code, $color, $id, $class_name, $modal_id) {
+        return '<button id="'.$id.'" class="'.$class_name.' '.$icon_code.' btn '.$color.'" data-toggle="modal" data-target="#'.$modal_id.'"></button>';
+    }
+
+    public static function controlImage($link) {
+        return "<img src='$link' class='img img-thumbnail' alt='Could not load image' />";
+    }
+
+}
