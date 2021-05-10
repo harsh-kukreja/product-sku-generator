6cf58a9f1fa847254711554988f00658c95bb28d
Harsh Kukreja <harshkukreja99@gmail.com>
2021-05-10 19:07:46 +0530

    refactor: modified the request file

--- app/Http/Requests/BaseProductRequest.php
+++ app/Http/Requests/BaseProductRequest.php
@@ -27,7 +27,6 @@ class BaseProductRequest extends FormRequest
         return [
             'product_name' => 'required|string',
             'product_price' => 'required|integer',
-            'product_stock' => 'required|integer',
             'product_description' => 'required|string',
             'is_variant' => 'required|in:'.BaseProduct::VARIANT.','.BaseProduct::NO_VARIANT,
             'product_image' => 'required|image|mimes:jpeg,png,jpg'
