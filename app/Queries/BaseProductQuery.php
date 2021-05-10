e684383cf6edaf97b3f735c0961c57817cb07bfc
Harsh Kukreja <harshkukreja99@gmail.com>
2021-05-10 22:22:06 +0530

    refactor: query fixed

--- app/Queries/BaseProductQuery.php
+++ app/Queries/BaseProductQuery.php
@@ -14,20 +14,6 @@ class BaseProductQuery {
      * @return Collection
      */
     public function getAllBaseProductsWithStock(): Collection {
-        return BaseProduct::query()
-            ->join('product_permutes', 'base_products.id', '=','product_permutes.base_product_id')
-            ->selectRaw('sum(product_permutes.stock) as stock,
-                base_products.name,
-                base_products.id,
-                base_products.price,
-                base_products.description,
-                base_products.image_url')
-            ->groupBy([ 'base_products.name' ,
-                'product_permutes.base_product_id',
-                'base_products.id',
-                'base_products.price',
-                'base_products.description',
-                'base_products.image_url'])
-            ->get();
+        return BaseProduct::all();
     }
 }
