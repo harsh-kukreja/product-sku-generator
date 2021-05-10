511bd6ff6e58bc449722502cfeee38c81ecb8ef9
Harsh Kukreja <harshkukreja99@gmail.com>
2021-05-10 06:15:34 +0530

    refactor: adding base product controller contants

---
+++ app/Http/Controllers/Constants/BaseProductControllerConstants.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Http\Controllers\Constants;
+
+
+interface BaseProductControllerConstants {
+    public const VARIANT = "1";
+    public const NO_VARIANT = "0";
+
+    public const PRODUCT_CREATED_SUCCESSFULLY = "Product Created SuccessFully";
+}
